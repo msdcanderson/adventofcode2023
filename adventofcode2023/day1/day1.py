a6829e9e266108c1635d5d4d4c2eb611d3d4a622
Michael Anderson <michael.anderson@banksearch-consultancy.com>
2023-12-02 23:20:42 +0000

    #6 it works!! wahey!!

--- adventofcode2023/day1/day1.py
+++ adventofcode2023/day1/day1.py
@@ -2,35 +2,72 @@ import re
 import logging
 from word2number import w2n
 
-# from input import input
+from input import input
+
 # from input_sample import input
 
 # Set up logging
 logging.basicConfig(
-    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+    level=logging.DEBUG, format="%(asctime)s - %(levelname)s - %(message)s"
 )
 
 
 class LineWithNumbersInAString:
-    def __init__(self, line: str, pattern: str) -> None:
+    def __init__(self, line: str) -> None:
         self.line = line
-        self.pattern = pattern
+        self.pattern_digits: str = r"(\d+)"
+        self.pattern_numbers_as_words: str = (
+            r"(one|two|three|four|five|six|seven|eight|nine)"
+        )
+        self.line_as_digits: str = self.convert_word_to_digit()
         self.numbers_in_line: list[str] = self.list_numbers_in_line()
         self.first_word: str = self.numbers_in_line[0]
         self.last_word: str = self.numbers_in_line[-1]
 
     def __str__(self) -> str:
-        return self.line
+        return f"Line: {self.line}, Line as digits: {self.line_as_digits}, Numbers in line: {self.numbers_in_line}, First word: {self.first_word}, Last word: {self.last_word}"
 
     def list_numbers_in_line(self) -> list[str]:
-        numbers = re.findall(self.pattern, self.line)
+        logging.debug(f"List numbers in line: {self.line_as_digits}")
+        numbers = re.findall(self.pattern_digits, self.line_as_digits)
         logging.debug(f"Numbers found in line: {numbers}")
         return numbers
 
+    def convert_word_to_digit(self) -> str:
+        line_as_digits = self.line
+        for x in range(len(self.line) - 3):
+            match = re.search(self.pattern_numbers_as_words, line_as_digits)
+            logging.debug(f"Match: {match}")
+            if match:
+                # Replace the first occurrence of the matched pattern
+                first_match = match.group(0)
+
+                first_match_digit = str(w2n.word_to_num(first_match))
+                logging.debug(
+                    f"First match: {first_match}, converted to: {first_match_digit}"
+                )
+
+                match_minus_a_char = first_match[1:]
+                logging.debug(f"Match minus a char: {match_minus_a_char}")
+
+                line_as_digits = line_as_digits.replace(
+                    first_match, first_match_digit + match_minus_a_char, 1
+                )
+                logging.debug(
+                    f"First match: {first_match}, converted to: {first_match_digit}"
+                )
+                logging.debug(f"Line as digits: {line_as_digits}")
+
+            else:
+                break
+
+        logging.info(f"Line: {line_as_digits}")
+        return line_as_digits
+
 
 class LineProcessor(LineWithNumbersInAString):
-    def __init__(self, line: str, pattern: str) -> None:
-        super().__init__(line, pattern)
+    def __init__(self, line: str) -> None:
+        super().__init__(line)
 
     @staticmethod
     def word_or_digit_to_digit(word: str, position: int) -> str:
@@ -59,21 +96,21 @@ class LineProcessor(LineWithNumbersInAString):
 
 
 def main() -> None:
-    lines = [
-        "atwo3b69",
-        "twooneightgt",
-        "ddgjgcrssevensix37twooneightgt",
-        "23eightptpspjtbnninesixfivedhfnmqjd",
-    ]
+    # lines = [
+    #     "atwo3b69",
+    #     "twooneightgt",
+    #     "ddgjgcrssevensix37twooneightgt",
+    #     "23eightptpspjtbnninesixfivedhfnmqjd",
+    # ]
     # convert input to a list of lines
-    # lines: list[str] = input.splitlines()
-    pattern: str = r"(\d+|one|two|three|four|five|six|seven|eight|nine)"
+    lines: list[str] = input.splitlines()
 
     sum_calibration_value: int = 0
     for line in lines:
         logging.info(f"Line: {line}")
 
-        line_processor = LineProcessor(line, pattern)
+        line_processor = LineProcessor(line)
+        logging.debug(f"Line processor: {line_processor}")
 
         calibration_value = line_processor.process()
         logging.info(f"Calibration Value: {calibration_value}")
--- tests/test_day1.py
+++ tests/test_day1.py
@@ -1,23 +1,46 @@
-import pytest
-from adventofcode2023.day1.day1 import LineWithNumbersInAString
+# import pytest
+# from adventofcode2023.day1.day1 import LineWithNumbersInAString
+# # from adventofcode2023.day1.day1 import LineProcessor
 
 
-class TestLineWithNumbersInAString:
-    pattern: str = r"(\d+|one|two|three|four|five|six|seven|eight|nine)"
+# # class TestLineWithNumbersInAString:
+# #     pattern: str = r"(\d+|one|two|three|four|five|six|seven|eight|nine)"
 
-    @pytest.mark.parametrize(
-        "line, expected",
-        [
-            # Add test cases here:
-            ("twooneightgt", ["two", "one", "eight"]),
-            (
-                "ddgjgcrssevensix37twooneightgt",
-                ["seven", "six", "37", "two", "one", "eight"],
-            ),
-            ("eightwothree", ["eight", "two", "three"]),
-            ("29", ["29"]),
-        ],
-    )  # type: ignore
-    def test_list_numbers_in_line(self, line: str, expected: list[str]) -> None:
-        line_with_numbers = LineWithNumbersInAString(line, self.pattern)
-        assert line_with_numbers.list_numbers_in_line() == expected
+# #     @pytest.mark.parametrize(
+# #         "line, expected",
+# #         [
+# #             # Add test cases here:
+# #             ("twooneightgt", ["two", "one", "eight"]),
+# #             (
+# #                 "ddgjgcrssevensix37twooneightgt",
+# #                 ["seven", "six", "37", "two", "one", "eight"],
+# #             ),
+# #             ("eightwothree", ["eight", "two", "three"]),
+# #             ("29", ["29"]),
+# #             ("a1b", ["1"])
+# #         ],
+# #     )  # type: ignore
+# #     def test_list_numbers_in_line(self, line: str, expected: list[str]) -> None:
+# #         line_with_numbers = LineWithNumbersInAString(line, self.pattern)
+# #         assert line_with_numbers.list_numbers_in_line() == expected
+
+# class TestLineProcessor:
+#     # pattern: str = r"(one|two|three|four|five|six|seven|eight|nine)"
+
+#     @pytest.mark.parametrize(
+#         "line, expected",
+#         [
+#             # Add test cases here:
+#             ("twooneightgt", "2wo1n8ightgt"),
+#             (
+#                 "ddgjgcrssevensix37twooneightgt",
+#                 "ddgjgcrs7even6ix372wo1n8ightgt",
+#             ),
+#             # ("eightwothree", ["eight", "two", "three"]),
+#             # ("29", ["29"]),
+#             # ("a1b", ["1"])
+#         ],
+#     )  # type: ignore
+#     def test_convert_word_to_digit(self, line: str, expected: list[str]) -> None:
+#         line_with_numbers = LineWithNumbersInAString(line)
+#         assert line_with_numbers.convert_word_to_digit() == expected

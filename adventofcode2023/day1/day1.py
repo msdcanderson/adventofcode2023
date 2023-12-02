7c637811d45d3149cbd050742d0d959bbc2913a1
Michael Anderson <michael.anderson@banksearch-consultancy.com>
2023-12-02 02:20:30 +0000

    #6 edge case breaks it!

--- adventofcode2023/day1/day1.py
+++ adventofcode2023/day1/day1.py
@@ -1,7 +1,8 @@
 import re
 from typing import List
 from word2number import w2n
-from input import input
+
+# from input import input
 
 # from input_sample import input
 
@@ -18,7 +19,10 @@ class LineWithNumbersInAString:
         return self.line
 
     def list_numbers_in_line(self) -> List[str]:
-        return re.findall(self.pattern, self.line)
+        something = re.findall(self.pattern, self.line)
+        # return re.findall(self.pattern, self.line)
+        print(something)
+        return something
 
 
 def word_or_digit_to_digit(word: str, position: int) -> str:
@@ -32,16 +36,24 @@ def word_or_digit_to_digit(word: str, position: int) -> str:
 def process_line(line: str, pattern: str) -> int:
     line_object = LineWithNumbersInAString(line, pattern)
     first_digit: str = word_or_digit_to_digit(line_object.first_word, 0)
+    print(first_digit)
     last_digit: str = word_or_digit_to_digit(line_object.last_word, -1)
+    print(last_digit)
     calibration_value: int = int(first_digit + last_digit)
     return calibration_value
 
 
 def main() -> None:
     # lines = ["atwo3b69","a1b"]
+    lines = [
+        "twooneightgt",
+        "ddgjgcrssevensix37twooneightgt",
+        "23eightptpspjtbnninesixfivedhfnmqjd",
+    ]
     # convert input to a list of lines
-    lines: list[str] = input.splitlines()
+    # lines: list[str] = input.splitlines()
     pattern: str = r"(\d+|one|two|three|four|five|six|seven|eight|nine)"
+
     sum_calibration_value: int = 0
     for line in lines:
         calibration_value = process_line(line, pattern)
